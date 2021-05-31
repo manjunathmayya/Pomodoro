e576e885c004072f0ef809d96513efd777ac7a28
Manjunath K <manjunathmayya@gmail.com>
2021-05-31 22:25:11 +0530

    Refactored code

--- Pomodoro/Report.cs
+++ Pomodoro/Report.cs
@@ -8,6 +8,82 @@ using System.Globalization;
 
 namespace Pomodoro
 {
+    public abstract class ReportTemplate
+    {
+        public DataTable dtReport = new DataTable();
+
+        public ReportTemplate()
+        {
+            CreateDataTable();
+        }
+         
+        
+        private void CreateDataTable() 
+        {
+            dtReport.Columns.Add("Task");
+            dtReport.Columns.Add("TimeTaken");
+        }
+
+        protected static string seconds_to_minute(int timeTaken)
+        {
+            int seconds = timeTaken % 60;
+            int minutes = timeTaken / 60;
+            string time = minutes + ":" + seconds.ToString("00");
+            return time;
+        }
+
+        public abstract DataTable GetReportData(DataTable dtTaskInfo);       
+
+    }
+
+    public class TaskTimeReport:ReportTemplate
+    {
+        public override DataTable GetReportData(DataTable dtTaskInfo)
+        {
+            var query = from row in dtTaskInfo.AsEnumerable()
+                        group row by row.Field<string>("Task") into grp
+                        select new
+                        {
+                            Task = grp.Key,
+                            sum = grp.Sum(r => Convert.ToInt32(r.Field<string>("TimeTaken") == "" ? 0 : r.Field<string>("TimeTaken")))
+                        };
+            foreach (var grp in query)
+            {
+                dtReport.Rows.Add(grp.Task, seconds_to_minute(grp.sum));
+            }
+
+            return dtReport;
+        }
+    }
+
+    public class DayWiseReport : ReportTemplate
+    {
+        public override DataTable GetReportData(DataTable dtTaskInfo)
+        {
+            var groups = dtTaskInfo.AsEnumerable().GroupBy(row => Convert.ToDateTime(row["Time"]).ToString("dd-MMM-yy"));
+
+            foreach (var group in groups)
+            {
+                var groups2 = group.OrderBy(row => row["Time"]).GroupBy(row => row["Task"]);
+
+                var timeTaken = group.Sum(r => Convert.ToInt32(r.Field<string>("TimeTaken") == "" ? 0 : r.Field<string>("TimeTaken")));
+
+                dtReport.Rows.Add("", "");
+                dtReport.Rows.Add(group.Key, seconds_to_minute(timeTaken));
+                dtReport.Rows.Add("", "");
+
+                foreach (var group2 in groups2)
+                {
+                    timeTaken = group2.Sum(r => Convert.ToInt32(r.Field<string>("TimeTaken") == "" ? 0 : r.Field<string>("TimeTaken")));
+                    dtReport.Rows.Add(group2.Key, seconds_to_minute(timeTaken));
+                }
+            }
+
+            return dtReport;
+        }
+    }
+
+
     public partial class Report : Form
     {
         private string log_file = "C:\\pomodoro_log_file.txt";
@@ -46,25 +122,8 @@ namespace Pomodoro
 
         private void lblTaskTimeReport_MouseClick(object sender, MouseEventArgs e)
         {
-            DataTable dtReport = new DataTable();
-            dtReport.Columns.Add("Task");
-            dtReport.Columns.Add("TimeTaken");
-            var query = from row in dtTaskInfo.AsEnumerable()
-                        group row by row.Field<string>("Task") into grp
-                        select new
-                        {
-                            Task = grp.Key,
-                            sum = grp.Sum(r => Convert.ToInt32(r.Field<string>("TimeTaken")== "" ? 0: r.Field<string>("TimeTaken")))
-                        };
-            foreach (var grp in query)
-            {
-                int seconds = grp.sum % 60;
-                int minutes = grp.sum / 60;
-                string time = minutes + ":" + seconds.ToString("00");
-                dtReport.Rows.Add(grp.Task, time);
-            }
-
-            grdTaskInfo.DataSource = dtReport;
+            TaskTimeReport taskTimeReport = new TaskTimeReport();
+            grdTaskInfo.DataSource = taskTimeReport.GetReportData(dtTaskInfo);
 
         }
 
@@ -75,43 +134,10 @@ namespace Pomodoro
 
         private void lblDayWiseReport_MouseClick(object sender, MouseEventArgs e)
         {
-            DataTable dtReport = new DataTable();
-            dtReport.Columns.Add("Task");
-            dtReport.Columns.Add("TimeTaken");
-
-            var groups = dtTaskInfo.AsEnumerable().GroupBy(row => Convert.ToDateTime(row["Time"]).ToString("dd-MMM-yy"));
-
-            foreach(var group in groups)
-            {
-                var groups2 = group.OrderBy(row => row["Time"]).GroupBy(row => row["Task"]);
-
-
-
-                var sum = group.Sum(r => Convert.ToInt32(r.Field<string>("TimeTaken") == "" ? 0 : r.Field<string>("TimeTaken")));
-                int seconds = sum % 60;
-                int minutes = sum / 60;
-                string time = minutes + ":" + seconds.ToString("00");
-
-
-
-                dtReport.Rows.Add("", "");
-                dtReport.Rows.Add( group.Key, time);
-                dtReport.Rows.Add("", "");
-
-                foreach (var group2 in groups2)
-                {
-                    sum = group2.Sum(r => Convert.ToInt32(r.Field<string>("TimeTaken") == "" ? 0 : r.Field<string>("TimeTaken")));
-
-                    seconds = sum % 60;
-                    minutes = sum / 60;
-                    time = minutes + ":" + seconds.ToString("00");
-
-
-                    dtReport.Rows.Add(group2.Key, time);
-                }
-            }
-
-            grdTaskInfo.DataSource = dtReport;
+            DayWiseReport dayWiseReport = new DayWiseReport();
+            grdTaskInfo.DataSource = dayWiseReport.GetReportData(dtTaskInfo);
         }
+
+        
     }
 }
